6ad4cc9c03485b8ec725db578d05fe0d4cbf0d4d
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-05-13 23:18:32 -0300

    Create layout component which serves as a wrapper for children (anything else it wraps around) - initially its just header

--- components/index.js
+++ components/index.js
@@ -1,3 +1,8 @@
+//General components
 export { PostCard } from './PostCard'
 export { PostWidget } from './PostWidget'
 export { Categories } from './Categories'
+
+//Layout components
+export { Header } from './layout/Header'
+export { Layout } from './layout/Layout'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
+import React, { useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
 
+import { Layout } from '../components'
+
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
