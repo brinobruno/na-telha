52595c23615093f12b6d87a039eb825906f15676
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-05-13 23:01:46 -0300

    add global scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
+
+import '../styles/globals.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
