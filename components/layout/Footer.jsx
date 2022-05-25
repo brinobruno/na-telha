f7c2037f7f8b0231cff8e7ed02ed50abc935ebbb
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-05-24 23:57:33 -0300

    change background

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -7,7 +7,7 @@ export const Footer = () => {
   return (
     <>
       <div className="container mx-auto px-5 mb-8">
-        <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="border-b w-full inline-block border-gray-700 py-8">
           <div className="md:float-left block">
             <Link href="/">
               <span className="cursor-pointer font-bold text-4xl text-white">
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -14,7 +14,7 @@ export const Header = () => {
   return (
     <>
       <div className="container mx-auto px-5 mb-8">
-        <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="border-b w-full inline-block border-gray-700 py-8">
           <div className="md:float-left block">
             <Link href="/">
               <span className="cursor-pointer font-bold text-4xl text-white">
@@ -34,10 +34,6 @@ export const Header = () => {
                 </Link>
               ))
             }
-              <span
-                className="md:float-right mt-2 align-middle text-white ml-4 font-semibold">
-                Explorar:
-              </span>
           </div>
         </div>
       </div>
