f1adef1bc5b6e313838fcd1fc7cd97d9256430f3
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-02 16:56:39 -0300

    newsletter form emailjs

--- components/NewsletterForm.jsx
+++ components/NewsletterForm.jsx
@@ -1,23 +1,46 @@
 import React from 'react'
+import emailjs from 'emailjs-com'
 
 export const NewsletterForm = () => {
+  const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID
+  const emailTemplate = process.env.NEXT_PUBLIC_EMAILJS_EMAIL_TEMPLATE
+  const APIKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_API_KEY
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+
+    emailjs.sendForm(serviceId, emailTemplate, e.target, APIKey)
+      .then(result => console.log(result.text))
+      .catch(err => console.log(err))
+
+      //form reset
+      e.target.reset() 
+  }
+
   return (
     <>
       <div>
         <h3 className='text-white pb-6'>
           Inscreva-se na Newsletter do NaTelha para receber notificações de novos posts!
         </h3>
-        <form class="w-full">
+        <form
+          onSubmit={ handleFormSubmit }
+          className="w-full"
+        >
           <div class="flex items-center border-b border-[#ff914d] py-2">
             <input
-              class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none text-md"
-              type="email"
+              className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none text-md"
+              type='email'
               id='email'
               name='email'
-              placeholder="Endereço e-mail"
-              aria-label="Endereço e-mail"
+              placeholder='Endereço e-mail'
+              aria-label='Endereço e-mail'
+              required
             />
-            <button class="flex-shrink-0 font-semibold bg-[#ff914d] hover:bg-[#d47133] border-[#ff914d] hover:border-[#d47133] text-md border-4 text-white py-1 px-2 rounded" type="button">
+            <button
+              className="flex-shrink-0 font-semibold bg-[#ff914d] hover:bg-[#d47133] border-[#ff914d] hover:border-[#d47133] text-md border-4 text-white py-1 px-2 rounded"
+              type='submit'
+            >
               Inscreva-se
             </button>
           </div>
