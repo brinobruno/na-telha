38ef6adcad2a7c39481f754585f8665f181c99c2
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-10 17:41:05 -0300

    refactor menu for mobile display

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,53 +1,72 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+
+import { RiMenu3Fill } from 'react-icons/ri'
 
 import { getCategories } from '../../services'
 import NatelhaLogo from '../../public/natelha-logo.png'
 
 export const Header = () => {
   const [categories, setCategories] = useState([])
+  const [isActive, setIsActive] = useState(false)
 
   useEffect(() => {
     getCategories()
       .then((newCategories) => setCategories(newCategories))
   }, [])
 
+  const toggleMenu = () => {
+    setIsActive(!isActive)
+  }
+
   return (
     <>
-      <div className="container mx-auto px-5 mb-4">
-        <div className="w-full inline-block py-8">
-          <div className="flex items-center justify-between">
-            <div>
-              <Link href="/">
-                <a>
-                  <Image
-                    width='150px'
-                    height='45.75px'
-                    className='cursor-pointer'
-                    src={ NatelhaLogo }
-                    alt='Na Telha'
-                  />
-                </a>
-              </Link>
-            </div>
-            <div className="visible">
-              <div className="">
-                {
-                  categories.map((category) => (
-                    <Link key={ category.slug } href={`/category/${category.slug}`}>
-                      <span
-                      className="mt-[1.65rem] align-middle text-white transition duration-300 hover:text-[#ff914d] ml-4 font-semibold cursor-pointer">
-                        { category.name }
-                      </span>
-                    </Link>
-                  ))
-                }
-              </div>
-            </div>
+    <nav className="container mx-auto px-5 mb-4">
+      <div className="container px-2 md:px-5 mx-auto md:flex md:justify-between md:items-center w-full inline-block py-8">
+        <div className="flex items-center justify-between">
+          <div>
+            <Link href="/">
+              <a>
+                <Image
+                  width='150px'
+                  height='45.75px'
+                  className='cursor-pointer'
+                  src={ NatelhaLogo }
+                  alt='Na Telha'
+                />
+              </a>
+            </Link>
+          </div>
+                
+          <div className="flex md:hidden">
+            <button
+              onClick={ toggleMenu }
+              type="button"
+              className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+              aria-label="toggle menu"
+            >
+              <RiMenu3Fill />
+            </button>
+          </div>
+        </div>
+
+        <div className={`items-center md:flex ${ isActive ? 'block absolute bg-[#131516] z-10 h-100 w-screen pb-16 pt-4 -translate-x-[39px] overflow-hidden' : 'hidden' }`}>
+          <div className={`flex flex-col md:flex-row`}>
+            {
+              categories.map((category) => (
+                <Link key={ category.slug } href={`/category/${category.slug}`}>
+                  <span
+                  className="text-xl md:text-base mt-[1.65rem] align-middle text-white transition duration-300 hover:text-[#ff914d] ml-10 md:ml-4 font-semibold cursor-pointer">
+                    { category.name }
+                  </span>
+                </Link>
+              ))
+            }
           </div>
         </div>
       </div>
+    </nav>
     </>
   )
 }
