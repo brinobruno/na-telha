12bcbb57415d94f9a915d3eb8d4aa95e6f8fbadf
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-14 21:25:59 -0300

    fix: Add object-fit cover to related/recent posts image to have a better view/resolution regardless of the image (icon) used

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -32,7 +32,7 @@ export const PostWidget = ({ categories, slug }) => {
             <div className='w-16 flex-none'>
               <div>
               <Image
-                className='related-posts-image align-middle rounded-full'
+                className='related-posts-image align-middle rounded-full object-cover'
                 src={ post.featuredImage.url }
                 alt={ post.title }
                 title={ post.title }
